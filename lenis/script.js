79dad6932887bb92dd1d5cdb5ef3b3679a2b94f6
anonymous298 <tackletalha@gmail.com>
2025-09-09 20:02:15 +0500

    lensi

---
+++ lenis/script.js
@@ -0,0 +1,16 @@
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
